844fea61188412f88506ad4dc2b8498cf49c7afd
lyj310 <yoenjii310@gmail.com>
2021-07-02 15:20:00 +0900

    21.07.02

--- ModuleExample_ex/config.js
+++ ModuleExample_ex/config.js
@@ -1,4 +1,36 @@
 module.exports = {
     server_port: 3000, 
-    db_url: 'mongodb://localhost:27017/local'
+    db_url: 'mongodb://localhost:27017/local',
+    db_schemas: [
+        { file: './user_schema'
+            , collection: 'users3'
+            , schemaName: 'UserSchema'
+            , modelName: 'UserModel'}
+    ],
+    route_info: [
+        //=====User=====//
+        {
+            // user.login
+            file: './user'
+            , path: '/process/login'
+            , method: 'login'
+            , type: 'post'
+        },  
+        {
+            // user.adduser
+            file: './user'
+            , path: '/process/adduser'
+            , method: 'adduser'
+            , type: 'post'
+            
+        },  
+        {
+            // user.listuser
+            file: './user'
+            , path: '/process/listuser'
+            , method: 'listuser'
+            , type: 'post'
+            
+        }
+    ]
 }
--- ModuleExample_ex/routes/user.js
+++ ModuleExample_ex/routes/user.js
@@ -36,14 +36,14 @@ var login = function(req, res) {
 				res.write('<h1>로그인 성공</h1>');
 				res.write('<div><p>사용자 아이디 : ' + paramId + '</p></div>');
 				res.write('<div><p>사용자 이름 : ' + username + '</p></div>');
-				res.write("<br><br><a href='/public/login.html'>다시 로그인하기</a>");
+				res.write("<br><br><a href='/login.html'>다시 로그인하기</a>");
 				res.end();
 			
 			} else {  // 조회된 레코드가 없는 경우 실패 응답 전송
 				res.writeHead('200', {'Content-Type':'text/html;charset=utf8'});
 				res.write('<h1>로그인  실패</h1>');
 				res.write('<div><p>아이디와 패스워드를 다시 확인하십시오.</p></div>');
-				res.write("<br><br><a href='/public/login.html'>다시 로그인하기</a>");
+				res.write("<br><br><a href='/login.html'>다시 로그인하기</a>");
 				res.end();
 			}
 		});
